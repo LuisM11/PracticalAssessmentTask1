7754adea05dbeb0a88d4c3cf16d816221038b179
LuisM11 <lumarinb@unal.edu.co>
2023-12-14 21:59:43 -0500

    Login model

---
+++ src/test/java/model/LoginCredentials.java
@@ -0,0 +1,19 @@
+package model;
+
+public class LoginCredentials {
+    private String email;
+    private String password;
+
+    public LoginCredentials(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+}
