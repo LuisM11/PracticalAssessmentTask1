2763829092101810e655a7bba78819cfe755541d
LuisM11 <lumarinb@unal.edu.co>
2023-12-15 00:19:22 -0500

    Attribute 'name' added

--- src/test/java/model/LoginCredentials.java
+++ src/test/java/model/LoginCredentials.java
@@ -3,10 +3,15 @@ package model;
 public class LoginCredentials {
     private String username;
     private String password;
+    private String name;
 
     public LoginCredentials(String username, String password){
         this.username = username;
         this.password = password;
+    }
+    public LoginCredentials(String username, String password, String name){
+        this(username, password);
+        this.name = name;
     }
 
     public String getUsername(){
@@ -15,5 +20,9 @@ public class LoginCredentials {
 
     public String getPassword(){
         return password;
+    }
+
+    public String getName(){
+        return name;
     }
 }
